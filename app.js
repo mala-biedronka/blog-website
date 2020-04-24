b3c37e2526f5b6c1a07c1fb4b60963beb332b8bb
Iryna Predborska <iryna.predborska@gmail.com>
2020-04-24 13:33:56 +0200

    create both about and contact pages along with partials

--- app.js
+++ app.js
@@ -13,9 +13,17 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 app.get("/", function (req, res) {
-  res.render("home", {homeContent: homeStartingContent});
-})
+    res.render("home", {homePageContent: homeStartingContent});
+});
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.get("/about", function (req, res) {
+    res.render("about", {aboutPageContent: aboutContent});
+});
+
+app.get("/contact", function (req, res) {
+    res.render("contact", {contactPageContent: contactContent});
+});
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
