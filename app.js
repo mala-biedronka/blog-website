1b9e4e344f35a48ef19f9392873a6e6304c3758a
Iryna Predborska <iryna.predborska@gmail.com>
2020-04-24 14:48:01 +0200

    create a compose page along with partials create get request to the compose page

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.get("/contact", function (req, res) {
     res.render("contact", {contactPageContent: contactContent});
 });
 
+app.get("/compose", function (req, res) {
+    res.render("compose");
+})
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
